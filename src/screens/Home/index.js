4210feb76094015a2005dd678b4afcbba4f78405
babaiyu <bayuputra9a@gmail.com>
2019-05-19 22:48:57 +0700

    added sound, image, & generate apk config

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
-import styles from './styles'
-
-//redux
+import { View, Text, TouchableOpacity, Animated } from 'react-native'
+import * as Animatable from 'react-native-animatable'
 import { connect } from 'react-redux'
 import { globalAction } from '../../redux/actions/globalAction'
+import styles from './styles'
 
 class Home extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            total: this.props.data.pressed
+            total: this.props.data.pressed ? this.props.data.pressed : 0,
+            color: '#62B42C',
         }
+        this.springValue = new Animated.Value(0.3)
     }
 
     _onPress = () => {
@@ -21,14 +22,30 @@ class Home extends Component {
             pressed: this.state.total
         }
         this.props.dispatch(globalAction(payload))
+        this.spring()
+    }
+
+    spring = () => {
+        this.springValue.setValue(0.3)
+        Animated.spring(
+            this.springValue,
+            {
+                toValue: 1,
+                friction: 1
+            }
+        ).start()
     }
 
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.fontBold}>Total Panda Pressed: {this.props.data.pressed}</Text>
-                <TouchableOpacity onPress={this._onPress} style={styles.button}>
-                    <Text style={styles.textWhite}>CLICK ME!</Text>
+                <TouchableOpacity onPress={this._onPress}>
+                    {/* <Text style={styles.textWhite}>CLICK ME!</Text> */}
+                    <Animated.Image
+                        style={{ width: 227, height: 200, transform: [{ scale: this.springValue }] }}
+                        source={require('../../assets/img/panda.png')}
+                    />
                 </TouchableOpacity>
             </View>
         )
